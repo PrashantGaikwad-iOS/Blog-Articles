c23e4436f1ce3e66a19eeaaf3cb20ff7431989c9
Prashant Gaikwad <prashant.gaikwad@leftrightmind.com>
2020-04-29 16:54:43 +0530

    Basic MVVM project setup done

---
+++ Blogs/Utils/Utils.swift
@@ -0,0 +1,26 @@
+//
+//  Utils.swift
+//  Blogs
+//
+//  Created by Prashant Gaikwad on 29/04/20.
+//  Copyright © 2020 Prashant Gaikwad. All rights reserved.
+//
+
+import UIKit
+
+// Image extension to load image from Url
+extension UIImage {
+    public static func loadFrom(url: URL, completion: @escaping (_ image: UIImage?) -> ()) {
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url) {
+                DispatchQueue.main.async {
+                    completion(UIImage(data: data))
+                }
+            } else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+            }
+        }
+    }
+}
